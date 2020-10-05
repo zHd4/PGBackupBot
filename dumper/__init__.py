4616dbfaf5dae121658a04bb934778e9d9801ceb
zHd4 <computersystembase@gmail.com>
2020-10-05 23:09:08 +0300

    Implemented some methods and created class Dumper in dumper module

--- dumper/__init__.py
+++ dumper/__init__.py
@@ -1,10 +1,27 @@
+import os
 import subprocess
+from dumper import env, db
 
 
-def execute_shell_command(command):
-    return subprocess.run(command, stdout=subprocess.PIPE)
+# noinspection PyMethodMayBeStatic
+class Dumper:
+    def dump(self):
+        for db_name in db.get_databases(db.get_connection()):
+            pass
 
+    def dump_db(self, path, db_name):
+        if not env.check_root():
+            return None
 
-def dump_db(db_name):
-    backup_command = 'sudo -u postgres pg_dump %s > %s' % (db_name, db_name + '.backup')
-    execute_shell_command(backup_command.split(' '))
+        db_file_path = os.path.join(path, db_name + '.backup')
+        backup_command = 'sudo -u postgres pg_dump %s > %s' % (db_name, db_file_path)
+
+        self.execute_shell_command(backup_command.split(' '))
+
+        return db_file_path
+
+    def execute_shell_command(self, command):
+        return subprocess.run(command, stdout=subprocess.PIPE)
+
+    def check_path(self, path):
+        return os.path.exists(self, path)
