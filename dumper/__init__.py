6f5f35e12d8752431aef937273c5e51d4db8144d
zhd4 <computersystembase@gmail.com>
2020-10-06 08:08:55 +0300

    Added incapsulation to Dumper class in dumper module

--- dumper/__init__.py
+++ dumper/__init__.py
@@ -14,26 +14,26 @@ class Dumper:
         paths = []
 
         for db_name in db.get_databases(self.__db_conn):
-            path = self.dump_db(self.__path, db_name)
+            path = self.__dump_db(self.__path, db_name)
 
             if db_name is not None:
                 paths.append(path)
             else:
                 raise NoRootPrivilegesException()
 
-    def dump_db(self, path, db_name):
+    def __dump_db(self, path, db_name):
         if not env.check_root():
             return None
 
         db_file_path = os.path.join(path, db_name + '.backup')
         backup_command = 'sudo -u postgres pg_dump %s > %s' % (db_name, db_file_path)
 
-        self.execute_shell_command(backup_command.split(' '))
+        self.__execute_shell_command(backup_command.split(' '))
 
         return db_file_path
 
-    def execute_shell_command(self, command):
+    def __execute_shell_command(self, command):
         return subprocess.run(command, stdout=subprocess.PIPE)
 
-    def check_path(self, path):
+    def __check_path(self, path):
         return os.path.exists(path)
