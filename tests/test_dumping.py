51b4253df29da0c28513816f741902465c233518
zhd4 <computersystembase@gmail.com>
2020-10-06 09:13:35 +0300

    Created test_dumping.py

---
+++ tests/test_dumping.py
@@ -0,0 +1,24 @@
+import os
+import config
+import unittest
+from dumper import Dumper, db
+
+
+class TestDumpingDatabases(unittest.TestCase):
+    def test_dumpimg(self):
+        dump_handler = Dumper(config.backups_path, db.get_connection())
+        dumps = dump_handler.dump()
+
+        self.assertEqual(True, len(dumps) > 0, 'Dumps paths list is empty')
+
+        self.assertEqual(
+            True,
+            len(os.listdir(config.backups_path)) > 0,
+            '%s dir must be contains backups' % config.backups_path
+        )
+
+        dump_handler.clear()
+
+
+if __name__ == '__main__':
+    unittest.main()
