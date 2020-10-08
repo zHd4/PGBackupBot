d3046dfdb667a5dfc0e78f64e8a646810e64774e
zhd4 <computersystembase@gmail.com>
2020-10-08 14:47:03 +0300

    Added RSA public key in config.py

--- config.py
+++ config.py
@@ -5,3 +5,22 @@ pg_login = 'backup_service'
 pg_password = 'op9iu8yut5'
 
 backups_path = '/tmp/pg_backup_bot_temp'
+
+key_length = 4096
+
+public_key = '''
+-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA4q48SRjyKXkD6LsCSdGJ
+VJlBW2ySmcgAzVa+YTysDWQZmC2HwVlsQpIUbqGvv+rCQxdXfq6Cjj6dnLJApaZd
+LdEU9r+vPA3Zh//iqXeUm6g1IL6tXBjVs4+isq93E6pfcANyAS1lAu/2DfvEe17f
+N6dECAAKmS+A5FdtdjN9Nb07ow+il6N8wpy7Tu8ZcUNfpWt3hu+/0ss5vh1Qe5it
+gqSw4CuLRMMsZjRwG/5s5RiWWbOBd7ejpqKdu7Jd0MTcLQwdLFggfkN/A9tW70Ux
+vHz7MYU4fgkxf5td5vvT1cqiNyPEhym8VJ8YlbAiKDNLTL3REYLgtHUAgto3+4KT
+zy+Hj7CSrqVQdIHyqFkxQe8YYE7KbPh/YAgU4lSW5RwobIO+LwOHYEf5CB5BFoc7
+BYw5eNkna3NdHMsyH5WuazIKw2fcpop04ZOI0DIg63gI5CbMn0dOEXvM3dSmLk1f
+10JxnR/9Evczb88/PPWqNt3S8iEEtHp99EGKFRMUZZWIadBtx01RZ6MyGX5P4TS7
+mUBWf9MhexSVK+ntxuaFIJKwj0KGVjCuRRo86yBdYqHSBWafB0b9MLEhQcFnOT8e
+Z4KCc8ZqPcLzZFQO42OJQ0QQA4ff+iSjxCAqya+lUoWtTYU7sC1v528q7zK7Yexe
+8HAmsw/UgKyKD/XO/0Uo2XkCAwEAAQ==
+-----END PUBLIC KEY-----
+'''
