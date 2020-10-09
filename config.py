4fd548d931c651cecff6016bed61280a49dc4f6f
zHd4 <computersystembase@gmail.com>
2020-10-09 19:31:00 +0300

    Configured config.py for telegram bot

--- config.py
+++ config.py
@@ -6,21 +6,30 @@ pg_password = 'op9iu8yut5'
 
 backups_path = '/tmp/pg_backup_bot_temp'
 
-key_length = 4096
+# noinspection SpellCheckingInspection
+bot_token = '1339540081:AAH6NKlRmwVp21lcEdvh3DlFZCCFdEVrK0I'
+user_chat_id = 461916456
 
-public_key = '''
+backup_interval_hour = 1
+
+enable_rsa_encryption = True
+
+rsa_key_length = 4096
+
+# noinspection SpellCheckingInspection
+rsa_public_key = '''
 -----BEGIN PUBLIC KEY-----
-MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA4q48SRjyKXkD6LsCSdGJ
-VJlBW2ySmcgAzVa+YTysDWQZmC2HwVlsQpIUbqGvv+rCQxdXfq6Cjj6dnLJApaZd
-LdEU9r+vPA3Zh//iqXeUm6g1IL6tXBjVs4+isq93E6pfcANyAS1lAu/2DfvEe17f
-N6dECAAKmS+A5FdtdjN9Nb07ow+il6N8wpy7Tu8ZcUNfpWt3hu+/0ss5vh1Qe5it
-gqSw4CuLRMMsZjRwG/5s5RiWWbOBd7ejpqKdu7Jd0MTcLQwdLFggfkN/A9tW70Ux
-vHz7MYU4fgkxf5td5vvT1cqiNyPEhym8VJ8YlbAiKDNLTL3REYLgtHUAgto3+4KT
-zy+Hj7CSrqVQdIHyqFkxQe8YYE7KbPh/YAgU4lSW5RwobIO+LwOHYEf5CB5BFoc7
-BYw5eNkna3NdHMsyH5WuazIKw2fcpop04ZOI0DIg63gI5CbMn0dOEXvM3dSmLk1f
-10JxnR/9Evczb88/PPWqNt3S8iEEtHp99EGKFRMUZZWIadBtx01RZ6MyGX5P4TS7
-mUBWf9MhexSVK+ntxuaFIJKwj0KGVjCuRRo86yBdYqHSBWafB0b9MLEhQcFnOT8e
-Z4KCc8ZqPcLzZFQO42OJQ0QQA4ff+iSjxCAqya+lUoWtTYU7sC1v528q7zK7Yexe
-8HAmsw/UgKyKD/XO/0Uo2XkCAwEAAQ==
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAvMBeyMibFmeceF+niq6X
+yvwm7hfjZXidBtomJrqjHb1i5GydgwxwYtz5Areg6Dz4Bh4hrniPiQ5GHBNAwR3s
+8o/GBzTQrtrnzNAWqn9ooRWv8MZFk7MYOIf+tXJzYv0zndwhBAVPkRXK14tHNmno
+ZvvoxCK9SIkSZtnl7GnjRJrMHfqdI+CXyiuYYND7LS/BRbqVBqBtiyQkJHiXgki5
+rGXKOJMdDbrN+PqDPj4dJFZrBDpXy3lk8fQIfb3r5shOQxlDlP9vbUIJIzMw2axE
+tJXTCB0AWINsRlApwg3VgkztBsB2IL4QGQufR2jekrxzpREWyv9cIkf2dCJ0qcae
+fPsrSd+YjcBNLellX9i9TdLDNOcca+inxAxaGdMGVM6/duGDk+PuF63pQdn9d/IX
+qsAHqUDYPLfokMuG0eGFyuOnfmDdfe6VQvDtmAxVlTd10Tu2mLQ1JpnpNZ82ZPGQ
+lsp006HeJygEcmllKY73I2lSJA07zng9sPz2eBZeMNexC8104ScHgJxOrCT6Kmw6
+7vl2SD8OqRXcB1AZXLNZa/Xd04qCtLSPCwkyV4j2pcvDhxlPFo2ULIAr8hbsWaSP
+5nopBZkfRr60zBKK0jmuRMo37R4VRSKZXs2Sz1MPkfkYbSokE49RmEYkGBzdNMt1
+w6kIdTjzvrZba2drg0RqCGkCAwEAAQ==
 -----END PUBLIC KEY-----
 '''
