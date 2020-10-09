0851e784e475a3831c4f3ffd52fd81e80928f7cb
zHd4 <computersystembase@gmail.com>
2020-10-09 23:30:02 +0300

    Fixed encrypt_file() in filecrypt.py

--- encryptor/filecrypt.py
+++ encryptor/filecrypt.py
@@ -2,8 +2,11 @@ from encryptor import Encryptor
 
 
 def encrypt_file(path, public_key, key_length):
-    with open(path, 'wb') as file:
+    with open(path, 'rb') as file:
         file_bytes = file.read()
         file_bytes = Encryptor(key_length, public_key).encrypt(file_bytes)
 
+        file.close()
+
+        file = open(path, 'wb')
         file.write(file_bytes)
