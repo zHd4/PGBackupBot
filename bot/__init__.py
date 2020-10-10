6111ff66fb9312e0faadb401a80037873ee239ae
zHd4 <computersystembase@gmail.com>
2020-10-10 23:33:15 +0300

    Changed public key configuring and fixed asyncio problems in bot __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,6 @@
 import time
 import config
+import asyncio
 from dumper import db
 from aiogram import Bot
 from dumper import Dumper
@@ -12,19 +13,26 @@ def start():
     dumper = Dumper(config.backups_path, db.get_connection())
 
     while True:
-        send_backups(bot, dumper)
+        loop = asyncio.get_event_loop()
+
+        try:
+            loop.run_until_complete(send_backups(bot, dumper))
+        finally:
+            loop.run_until_complete(loop.shutdown_asyncgens())
+            loop.close()
+
         time.sleep(config.backup_interval_hour * 60 * 60)
 
 
-def send_backups(bot, dumper):
+async def send_backups(bot, dumper):
     try:
         paths = dumper.dump()
 
         for path in paths:
             if config.enable_rsa_encryption:
-                filecrypt.encrypt_file(path, config.rsa_public_key.encode('utf-8'), config.rsa_key_length)
+                filecrypt.encrypt_file(path, open(config.rsa_public_key_path, 'rb').read(), config.rsa_key_length)
 
-            bot.send_document(config.your_chat_id, open(path, 'rb'))
+            await bot.send_document(config.your_chat_id, open(path, 'rb'))
             time.sleep(0.5)
 
         dumper.clear()
--- config.py
+++ config.py
@@ -17,19 +17,4 @@ enable_rsa_encryption = True
 rsa_key_length = 4096
 
 # noinspection SpellCheckingInspection
-rsa_public_key = '''
------BEGIN PUBLIC KEY-----
-MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAvMBeyMibFmeceF+niq6X
-yvwm7hfjZXidBtomJrqjHb1i5GydgwxwYtz5Areg6Dz4Bh4hrniPiQ5GHBNAwR3s
-8o/GBzTQrtrnzNAWqn9ooRWv8MZFk7MYOIf+tXJzYv0zndwhBAVPkRXK14tHNmno
-ZvvoxCK9SIkSZtnl7GnjRJrMHfqdI+CXyiuYYND7LS/BRbqVBqBtiyQkJHiXgki5
-rGXKOJMdDbrN+PqDPj4dJFZrBDpXy3lk8fQIfb3r5shOQxlDlP9vbUIJIzMw2axE
-tJXTCB0AWINsRlApwg3VgkztBsB2IL4QGQufR2jekrxzpREWyv9cIkf2dCJ0qcae
-fPsrSd+YjcBNLellX9i9TdLDNOcca+inxAxaGdMGVM6/duGDk+PuF63pQdn9d/IX
-qsAHqUDYPLfokMuG0eGFyuOnfmDdfe6VQvDtmAxVlTd10Tu2mLQ1JpnpNZ82ZPGQ
-lsp006HeJygEcmllKY73I2lSJA07zng9sPz2eBZeMNexC8104ScHgJxOrCT6Kmw6
-7vl2SD8OqRXcB1AZXLNZa/Xd04qCtLSPCwkyV4j2pcvDhxlPFo2ULIAr8hbsWaSP
-5nopBZkfRr60zBKK0jmuRMo37R4VRSKZXs2Sz1MPkfkYbSokE49RmEYkGBzdNMt1
-w6kIdTjzvrZba2drg0RqCGkCAwEAAQ==
------END PUBLIC KEY-----
-'''
+rsa_public_key_path = 'public.pem'
