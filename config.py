fa3c64455c5bd4cb28105a20dd77c5f7994b3e10
zHd4 <computersystembase@gmail.com>
2020-10-11 14:39:09 +0300

    Config reset

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
-pg_host = '127.0.0.1'
+pg_host = 'PostgreSQL server address'
 pg_port = 5432
 
-pg_login = 'backup_service'
-pg_password = 'op9iu8yut5'
+pg_login = 'PostgreSQL login'
+pg_password = 'PostgreSQL password'
 
 backups_path = '/tmp/pg_backup_bot_temp'
 
 # noinspection SpellCheckingInspection
-bot_token = '1339540081:AAH6NKlRmwVp21lcEdvh3DlFZCCFdEVrK0I'
-your_chat_id = 461916456
+bot_token = 'Telegram bot token'
+your_chat_id = 461916456  # your chat id
 
-backup_interval_hour = 1
+backup_interval_hour = 1  # Interval of backup in hours
 
-enable_rsa_encryption = True
+enable_rsa_encryption = False  # Enable RSA encryption if True
 
-rsa_key_length = 4096
+rsa_key_length = 4096  # RSA key length
 
 # noinspection SpellCheckingInspection
-rsa_public_key_path = 'public.pem'
+rsa_public_key_path = 'public.pem'  # Path to RSA public key
